11993b5327c1e5d5081ce6a17e8540654937c809
Brittany Rodebaugh <brittanyparker61214@gmail.com>
2023-03-12 21:12:29 -0400

    First Commit

---
+++ Animal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnowledgeCheck
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+    }
+
+    public class Dog : Animal 
+    {
+        public string Breed { get; set; }
+        public string FurColor { get; set;}
+        public int Age { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using KnowledgeCheck;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnowledgeCheck
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("How many records do you want to add? ");
+            var numberOfRecords = int.Parse(Console.ReadLine());
+
+            var recordList = new List<Dog>();
+            for (int i = 0; i < numberOfRecords; i++)
+            {
+                // In this loop, populate the object's properties using Console.ReadLine()
+                var myDog = new Dog();
+
+                Console.WriteLine("What's your Dog's name? ");
+                myDog.Name = Console.ReadLine();
+
+                Console.WriteLine($"What breed is {myDog.Name}?");
+                myDog.Breed = Console.ReadLine();
+
+                Console.WriteLine($"What color fur does {myDog.Name} have?");
+                myDog.FurColor = Console.ReadLine();
+
+                Console.WriteLine($"How old is {myDog.Name}?");
+                myDog.Age = int.Parse(Console.ReadLine());
+
+
+                recordList.Add(myDog);
+            }
+
+            // Print out the list of records using Console.WriteLine()
+            foreach(var myDog in recordList)
+            {
+                Console.WriteLine("*************************");
+                Console.WriteLine($" Name | {myDog.Name}");
+                Console.WriteLine($"Breed | {myDog.Breed}");
+                Console.WriteLine($"Color | {myDog.FurColor}");
+                Console.WriteLine($"  Age | {myDog.Age}");
+                Console.WriteLine("*************************");
+            }
+        }
+    }
+}
